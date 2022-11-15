2546b11f15fcea7eb213d12e0de880bb8dc9139f
paulo.tavares <paulo.tavares@olxbr.com>
2022-11-14 21:07:01 -0300

    first commit

---
+++ src/main/kotlin/com/higortavares/awsiam/AwsIamApplication.kt
@@ -0,0 +1,11 @@
+package com.higortavares.awsiam
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AwsIamApplication
+
+fun main(args: Array<String>) {
+	runApplication<AwsIamApplication>(*args)
+}
